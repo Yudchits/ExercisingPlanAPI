961a89d33663bab1910864128e0e46b284a2c5a6
Yudchits <pashka.yudchits@gmail.com>
2024-04-05 23:50:02 +0300

    ExercisingPlanRepository was implemented

---
+++ ExercisingPlanAPI/Repositories/ExercisingPlanRepository.cs
@@ -0,0 +1,85 @@
+﻿using ExercisingPlanAPI.Data;
+using ExercisingPlanAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExercisingPlanAPI.Repositories
+{
+    public class ExercisingPlanRepository : IExercisingPlanRepository
+    {
+        private readonly DataContext _context;
+
+        public ExercisingPlanRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateExercisingPlanAsync(ExercisingPlan plan)
+        {
+            await _context.AddAsync(plan);
+            var isCreated = await SaveChangesAsync();
+            return isCreated;
+        }
+
+        public async Task<bool> DeleteAccessToExercisingPlanAsync(UserExercisingPlan userPlans)
+        {
+            _context.UserExercisingPlans.Remove(userPlans);
+            var isDeleted = await SaveChangesAsync();
+            return isDeleted;
+        }
+
+        public async Task<bool> DeleteExercisingPlanAsync(ExercisingPlan plan)
+        {
+            _context.ExercisingPlans.Remove(plan);
+            var isDeleted = await SaveChangesAsync();
+            return isDeleted;
+        }
+
+        public async Task<bool> ExercisingPlanExistsAsync(int id)
+        {
+            return await _context.ExercisingPlans.AnyAsync(ep => ep.Id == id);
+        }
+
+        public async Task<ICollection<ExercisingPlan>> GetAllExercisingPlansAsync()
+        {
+            return await _context.ExercisingPlans.ToListAsync();
+        }
+
+        public async Task<ICollection<ExercisingPlan>> GetAvailableExercisingPlans(int userId)
+        {
+            return await _context.UserExercisingPlans.Where(uep => uep.UserId == userId).Select(uep => uep.ExercisingPlan).ToListAsync(); 
+        }
+
+        public async Task<ExercisingPlan> GetExercisingPlanByIdAsync(int id)
+        {
+            return await _context.ExercisingPlans.FirstOrDefaultAsync(ep => ep.Id == id);
+        }
+
+        public async Task<ICollection<ExercisingPlan>> GetExercisingPlansOfOwnerAsync(int ownerId)
+        {
+            return await _context.ExercisingPlans.Where(ep => ep.OwnerId == ownerId).ToListAsync();
+        }
+
+        public async Task<bool> MakeExercisingPlanAvailableForUserAsync(UserExercisingPlan userPlans)
+        {
+            await _context.UserExercisingPlans.AddAsync(userPlans);
+            var isSaved = await SaveChangesAsync();
+            return isSaved;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            var affectedRows = await _context.SaveChangesAsync();
+            return affectedRows > 0;
+        }
+
+        public async Task<bool> UpdateExercisingPlanAsync(ExercisingPlan plan)
+        {
+            _context.ExercisingPlans.Update(plan);
+            var isUpdated = await SaveChangesAsync();
+            return isUpdated;
+        }
+    }
+}
---
+++ ExercisingPlanAPI/Repositories/IExercisingPlanRepository.cs
@@ -0,0 +1,21 @@
+﻿using ExercisingPlanAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExercisingPlanAPI.Repositories
+{
+    public interface IExercisingPlanRepository
+    {
+        Task<ICollection<ExercisingPlan>> GetAllExercisingPlansAsync();
+        Task<ExercisingPlan> GetExercisingPlanByIdAsync(int id);
+        Task<ICollection<ExercisingPlan>> GetExercisingPlansOfOwnerAsync(int ownerId);
+        Task<ICollection<ExercisingPlan>> GetAvailableExercisingPlans(int userId);
+        Task<bool> MakeExercisingPlanAvailableForUserAsync(UserExercisingPlan userPlans);
+        Task<bool> DeleteAccessToExercisingPlanAsync(UserExercisingPlan userPlans);
+        Task<bool> CreateExercisingPlanAsync(ExercisingPlan plan);
+        Task<bool> UpdateExercisingPlanAsync(ExercisingPlan plan);
+        Task<bool> DeleteExercisingPlanAsync(ExercisingPlan plan);
+        Task<bool> ExercisingPlanExistsAsync(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}

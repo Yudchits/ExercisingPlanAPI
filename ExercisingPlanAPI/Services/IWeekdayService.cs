b57e876adc2fc51a33b2d6310597e30b53bcd2ae
Yudchits <pashka.yudchits@gmail.com>
2024-04-26 22:13:04 +0300

    WeekdayService was implemented

---
+++ ExercisingPlanAPI/Services/IWeekdayService.cs
@@ -0,0 +1,15 @@
+﻿using ExercisingPlanAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExercisingPlanAPI.Services
+{
+    public interface IWeekdayService
+    {
+        Task<ICollection<Weekday>> GetAllWeekdaysAsync();
+        Task<Weekday> GetWeekdayByIdAsync(int id);
+        Task<bool> InsertWeekdayAsync(Weekday weekday);
+        Task<bool> UpdateWeekdayAsync(Weekday weekday);
+        Task<bool> DeleteWeekdayByIdAsync(int id);
+    }
+}
---
+++ ExercisingPlanAPI/Services/WeekdayService.cs
@@ -0,0 +1,42 @@
+﻿using ExercisingPlanAPI.Models;
+using ExercisingPlanAPI.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExercisingPlanAPI.Services
+{
+    public class WeekdayService : IWeekdayService
+    {
+        private readonly IWeekdayRepository _repository;
+
+        public WeekdayService(IWeekdayRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ICollection<Weekday>> GetAllWeekdaysAsync()
+        {
+            return await _repository.GetAllWeekdaysAsync();
+        }
+
+        public async Task<Weekday> GetWeekdayByIdAsync(int id)
+        {
+            return await _repository.GetWeekdayByIdAsync(id);
+        }
+
+        public async Task<bool> InsertWeekdayAsync(Weekday weekday)
+        {
+            return await _repository.InsertWeekdayAsync(weekday);
+        }
+
+        public async Task<bool> UpdateWeekdayAsync(Weekday weekday)
+        {
+            return await _repository.UpdateWeekdayAsync(weekday);
+        }
+
+        public async Task<bool> DeleteWeekdayByIdAsync(int id)
+        {
+            return await _repository.DeleteWeekdayByIdAsync(id);
+        }
+    }
+}

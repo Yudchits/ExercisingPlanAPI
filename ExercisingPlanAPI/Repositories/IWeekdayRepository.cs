510b4acc9b715808904557b03b711a56a0185e34
Yudchits <pashka.yudchits@gmail.com>
2024-04-26 22:09:57 +0300

    WeekdayRepository was implemented

---
+++ ExercisingPlanAPI/Repositories/IWeekdayRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExercisingPlanAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExercisingPlanAPI.Repositories
+{
+    public interface IWeekdayRepository
+    {
+        Task<ICollection<Weekday>> GetAllWeekdaysAsync();
+        Task<Weekday> GetWeekdayByIdAsync(int id);
+        Task<bool> InsertWeekdayAsync(Weekday weekday);
+        Task<bool> UpdateWeekdayAsync(Weekday weekday);
+        Task<bool> DeleteWeekdayByIdAsync(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ ExercisingPlanAPI/Repositories/WeekdayRepository.cs
@@ -0,0 +1,52 @@
+﻿using ExercisingPlanAPI.Data;
+using ExercisingPlanAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExercisingPlanAPI.Repositories
+{
+    public class WeekdayRepository : IWeekdayRepository
+    {
+        private readonly DataContext _context;
+
+        public WeekdayRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Weekday>> GetAllWeekdaysAsync()
+        {
+            return await _context.Weekdays.ToListAsync();
+        }
+
+        public async Task<Weekday> GetWeekdayByIdAsync(int id)
+        {
+            return await _context.Weekdays.FirstOrDefaultAsync(weekday => weekday.Id == id);
+        }
+
+        public async Task<bool> InsertWeekdayAsync(Weekday weekday)
+        {
+            await _context.Weekdays.AddAsync(weekday);
+            return await SaveChangesAsync();
+        }
+
+        public async Task<bool> UpdateWeekdayAsync(Weekday weekday)
+        {
+            _context.Weekdays.Update(weekday);
+            return await SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteWeekdayByIdAsync(int id)
+        {
+            var weekday = await GetWeekdayByIdAsync(id);
+            _context.Remove(weekday);
+            return await SaveChangesAsync();
+        }
+        
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
